d4cf84fb4e125f22191f547522e070a9927cbaa4
fedemarino31 <fedemarino@gmail.com>
2021-03-13 16:45:44 -0300

    added sky texture

--- src/client/client.ts
+++ src/client/client.ts
@@ -11,6 +11,8 @@ const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
+
+
 // Lights
 const ambientLight = new THREE.HemisphereLight(new THREE.Color(0.3, 0.3, 0.5),
                                                new THREE.Color(0.8, 0.5, 0.1),
@@ -22,14 +24,30 @@ scene.add(ambientLight)
 
 // Camera
 const controls = new OrbitControls(camera, renderer.domElement)
+controls.target.set(-1, 0, 0)
+
 camera.position.set(10, 10, 10)
 
-
+let skyTextureLoader = new THREE.TextureLoader();
 const loader = new GLTFLoader();
 loader.load("/models/city.glb",
-            (gltf) => scene.add(gltf.scene),
+            (gltf) => {
+                scene.add(gltf.scene)
+                skyTextureLoader.load("maps/sky1.jpg", 
+                                      (texture) => gltf.scene.getObjectByName("Cielo").material.map=texture
+                )
+            },
             null,
             (err) => console.log(err))
+
+
+/*
+const skyGeo = new THREE.SphereBufferGeometry(200,64,64);
+const skyMat = new THREE.MeshBasicMaterial();
+
+const sky = new THREE.Mesh(skyGeo,skyMat);
+//scene.add(sky);
+*/
 
 const animate = function () {
 
